46e8834a11caaccdd152165c6666ba26dac77bb5
dianisia <dianisia.m@yandex.ru>
2021-02-23 23:23:29 +0300

    Expense panel improvements

--- FinanceTrack/NewExpenseViewController.swift
+++ FinanceTrack/NewExpenseViewController.swift
@@ -2,8 +2,19 @@ import UIKit
 
 class NewExpenseViewController: UIViewController {
 
+    @IBOutlet weak var reduceExpenseButton: UIButton!
+    @IBOutlet weak var enlargeExpenseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        customizeExpenseButton(button: reduceExpenseButton)
+        customizeExpenseButton(button: enlargeExpenseButton)
+    }
+    
+    func customizeExpenseButton(button: UIButton) {
+        button.layer.borderWidth = 2
+        button.layer.borderColor = Helper.UIColorFromHex(rgbValue: 0xEAEAF2).cgColor
+        button.layer.cornerRadius = 10
     }
     
 }

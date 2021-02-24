50b2ef3e979a4d8709f3c90c379ef31a51a92efe
dianisia <dianisia.m@yandex.ru>
2021-02-24 22:47:03 +0300

    New expense layout improvements

--- FinanceTrack/NewExpenseViewController.swift
+++ FinanceTrack/NewExpenseViewController.swift
@@ -5,16 +5,33 @@ class NewExpenseViewController: UIViewController {
     @IBOutlet weak var reduceExpenseButton: UIButton!
     @IBOutlet weak var enlargeExpenseButton: UIButton!
     
+    @IBOutlet weak var expenseLabel: UILabel!
+    
+    private var currentExpense = 500;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         customizeExpenseButton(button: reduceExpenseButton)
         customizeExpenseButton(button: enlargeExpenseButton)
+        expenseLabel.text = String(currentExpense)
     }
     
     func customizeExpenseButton(button: UIButton) {
         button.layer.borderWidth = 2
         button.layer.borderColor = Helper.UIColorFromHex(rgbValue: 0xEAEAF2).cgColor
         button.layer.cornerRadius = 10
+    }
+    
+    
+    @IBAction func onReduceTap(_ sender: Any) {
+        currentExpense -= 1
+        expenseLabel.text = String(currentExpense)
+    }
+    
+    
+    @IBAction func onEnlargeTap(_ sender: Any) {
+        currentExpense += 1
+        expenseLabel.text = String(currentExpense)
     }
     
 }
